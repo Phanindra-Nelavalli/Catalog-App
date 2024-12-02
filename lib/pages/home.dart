d05e689ec59f7304149968cc03b55e681b3c9e06
Phanindra-Nelavalli <nelavalliphanindra4@gmail.com>
2024-12-02 22:24:35 +0530

    Grid Layout

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -40,12 +40,52 @@ class _HomeState extends State<Home> {
         centerTitle: true,
       ),
       body: CatalogModel.items.isNotEmpty
-          ? ListView.builder(
-              padding: EdgeInsets.all(8.0),
-              itemCount: CatalogModel.items.length,
-              itemBuilder: (context, index) {
-                return ItemWidget(item: CatalogModel.items[index]);
-              },
+          // ? ListView.builder(
+          //   padding: EdgeInsets.all(8.0),
+          //   itemCount: CatalogModel.items.length,
+          //   itemBuilder: (context, index) {
+          //     return ItemWidget(item: CatalogModel.items[index]);
+          //   },
+          // )
+          ? Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Card(
+                clipBehavior: Clip.antiAlias,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
+                child: GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      mainAxisSpacing: 14,
+                      crossAxisSpacing: 6),
+                  itemBuilder: (context, index) {
+                    final item = CatalogModel.items[index];
+                    return GridTile(
+                      header: Container(
+                        padding: EdgeInsets.all(8),
+                        child: Text(
+                          item.name,
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        decoration: BoxDecoration(
+                          color: Colors.deepPurple,
+                        ),
+                      ),
+                      child: Container(
+                          margin: EdgeInsets.only(top: 50),
+                          child: Image.network(item.image)),
+                      footer: Text(
+                        item.price.toString(),
+                        style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.deepPurple,
+                            fontWeight: FontWeight.w800),
+                      ),
+                    );
+                  },
+                  itemCount: CatalogModel.items.length,
+                ),
+              ),
             )
           : Center(
               child: CircularProgressIndicator(),
